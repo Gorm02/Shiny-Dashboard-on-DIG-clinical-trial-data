1adedcce42f1a5f214b5bd72790c541293e4f1ac
Dylan House <Dylan.House02@pm.me>
2025-11-24 15:05:09 +0000

    Added R script to load the clean DIG dataset required for the shinyapp to function.

--- DIG_Dashboard/server.R
+++ DIG_Dashboard/server.R
@@ -15,8 +15,51 @@ library(dplyr)
 library(ggplot2)
 
 server <- function(input, output, session) {
+  
+  dig.df <- reactive({
+    df <- input$dig.df
+    
+    df$TRTMT <- factor(df$TRTMT,
+                       levels = c("Placebo", "Treatment"))
+    
+    df$SEX <- factor(df$SEX,
+                     levels = c(1, 2),
+                     labels = c("Male", "Female"))
+    
+    df$DEATH <- factor(df$DEATH,
+                       levels = c("FALSE", "TRUE"),
+                       labels = c("Alive", "Death"))
+    
+    df$HYPERTEN <- factor(df$HYPERTEN,
+                          levels = c("FALSE", "TRUE"),
+                          labels = c("No History of Hypertension",
+                                     "History of Hypertension"))
+    
+    df$CVD <- factor(df$CVD,
+                     levels = c("FALSE", "TRUE"),
+                     labels = c("No Cardiovascular Disease",
+                                "Cardiovascular Disease"))
+    
+    df$WHF <- factor(df$WHF,
+                     levels = c("FALSE", "TRUE"),
+                     labels = c("No Worsening Heart Failure",
+                                "Worsening Heart Failure"))
+    
+    df$DIG <- factor(df$DIG,
+                     levels = c("FALSE", "TRUE"),
+                     labels = c("No Digoxin Toxicity",
+                                "Digoxin Toxicity"))
+    
+    df$HOSP <- factor(df$HOSP,
+                      levels = c("FALSE", "TRUE"),
+                      labels = c("Not Hospitalized",
+                                 "Hospitalized"))
+    
+    df
+  })
   output$Baseline_Values_plot <- renderPlot({
-    ggplot(dig.df, aes(x = TRTMT, y = .data[[input$features]], fill = TRTMT)) +
+    df <- as.data.frame(dig.df())
+    ggplot(df, aes(x = TRTMT, y = .data[[input$features]], fill = TRTMT)) +
       geom_boxplot() +
       scale_fill_manual(values=c("seagreen2", "cornflowerblue")) +
       geom_jitter(alpha = 0.2) +
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Shiny-Dashboard-on-DIG-clinical-trial-data
+
+## Overview
+
+
+## How to use
+
+First, load the supplementary file 'Load_and_clean_data' and run the script. With this, you are now able to use the shiny app.
